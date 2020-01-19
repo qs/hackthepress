904a23ed8863c05422519e04bf7dc4ba615dcc2b
jerrysam <jeromeminney@gmail.com>
2020-01-19 14:46:42 +0000

    update the css classes to not clash with other webpages

--- ChromeExtensionQualityNews/content.user.js
+++ ChromeExtensionQualityNews/content.user.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     $("body").prepend( newsextensionSidebarPanel );
     $("body").prepend( arrow );
 
-    $('.newsextensionSidebarPanel').css({'font-family': '"Merriweather Sans", sans-serif', 'z-index': '999999', 'width':'300px', 'float':'left', 'height':'100%', 'background':'white', 'position':'absolute', 'left':'-300px'})
+    $('.newsextensionSidebarPanel').css({'font-family': '"Merriweather Sans", sans-serif', 'font-size': '14px', 'z-index': '999999', 'width':'300px', 'float':'left', 'height':'100%', 'background':'white', 'position':'absolute', 'left':'-300px'})
     $('.news-extension-sidebar-panel-slider-arrow').css({ 'box-sizing': 'border-box', 'z-index': '999999', 'padding':'4px 4px 4px 17px', 'width':'50px', 'height':'50px', 'border-radius': '25px', 'margin': '5px', 'float':'left', 'background':'black','font':'400 32px Arial, Helvetica, sans-serif', 'color':'white','text-decoration':'none', 'position':'absolute'})
 });
 
@@ -64,11 +64,11 @@ $(function(){
         var sidebarPanel = document.getElementsByClassName("newsextensionSidebarPanel")[0];
         var sidebarPanelTopContent = document.createElement("div");
         sidebarPanelTopContent.innerHTML = `
-        <div class="news-extension-sidebar-panel-top-text card-container">
+        <div class="news-extension-sidebar-panel-top-text alttab-spec-card-container">
             <p>The article you're reading has <br/><span style="//background-color: #e67e22;"> ${relatedArticles.warnings} quality warnings</span><br/> according to Glitched.News</p>
             <p><a target="_blank" style="text-decoration: underline; font-weight: 500;" href="${relatedArticles.analysis}">See a full analysis here.</a></p>
         </div>
-        <div class="news-extension-sidebar-panel-top-text card-container">
+        <div class="news-extension-sidebar-panel-top-text alttab-spec-card-container">
             <p>You'll find the same information from Alternative Tabloids below.<br/></p>
             <p><a target="_blank" style="text-decoration: underline; font-weight: 500;" href="https://www.reddit.com/r/politics/wiki/whitelist">Our filtering criteris can be found here.</a></p>
         </div>
@@ -78,21 +78,21 @@ $(function(){
           var sidebarPanel = document.getElementsByClassName("newsextensionSidebarPanel")[0];
           var div = document.createElement("div");
           div.innerHTML = `
-            <div class="article-container card-container">
+            <div class="alttab-spec-article-container alttab-spec-card-container">
             <a href="${relatedArticles.articles[i].url}" target="_blank">
-                <div class="article-data-left">
-                    <div class="source article-meta">
+                <div class="alttab-spec-article-data-left">
+                    <div class="source alttab-spec-article-meta">
                         <p>From <span class="publication">${relatedArticles.articles[i].source.name}</span></p>
                     </div>
-                    <div class="article-title">
+                    <div class="alttab-spec-article-title">
                         <!-- Need to limit the number of characters in the headline -->
-                        <p class="article-title-text">${relatedArticles.articles[i].title.substring(0,60)+"..."}</p>
+                        <p class="alttab-spec-article-title-text">${relatedArticles.articles[i].title.substring(0,60)+"..."}</p>
                     </div>
-                    <div class="article-date article-meta">
+                    <div class="article-date alttab-spec-article-meta">
                         <p>${relatedArticles.articles[i].displayDate}</p>
                     </div>
                 </div>
-                <div class="article-image-right">
+                <div class="alttab-spec-article-image-right">
                     <img src="${relatedArticles.articles[i].urlToImage}" alt="">
                 </div>
             </a>
@@ -115,46 +115,50 @@ $(function(){
     .news-extension-sidebar-panel-top-text p {
        margin: 0.5em;
     }
-    .wrapper {
+    .alttab-spec-wrapper {
         width: 400px;
         margin: auto;
         padding-top: 5px;
         background: #eee;
         height: 100vh;
     }
-    .card-container {
+    .alttab-spec-card-container {
         background-color: white;
         margin: 5px;
         border-radius: 5px;
         box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.5);
         overflow: hidden;
+        color: black;
     }
-    .article-container {
+    .alttab-spec-card-container a {
+        color: black;
+    }
+    .alttab-spec-article-container {
         height: 120px;
     }
-    .article-container p {
+    .alttab-spec-article-container p {
         margin: 0.2em;
     }
-    .article-meta {
+    .alttab-spec-article-meta {
         font-size: 0.8em;
         opacity: 0.7;
     }
-    .article-data-left {
+    .alttab-spec-article-data-left {
         width: 65%;
         float: left;
         padding: 5px;
     }
-    .article-title {
+    .alttab-spec-article-title {
         font-weight: bold;
     }
 
-    .article-image-right {
+    .alttab-spec-article-image-right {
         width: 35%;
         height: 100%;
         float: left;
         overflow: hidden;
     }
-    .article-image-right > img {
+    .alttab-spec-article-image-right > img {
         height: 100%;
         object-fit: cover;
     }
@@ -170,46 +174,50 @@ $(function(){
         .news-extension-sidebar-panel-top-text p {
             margin: 0.5em;
         }
-        .wrapper {
+        .alttab-spec-wrapper {
             width: 400px;
             margin: auto;
             padding-top: 5px;
             background: #eee;
             height: 100vh;
         }
-        .card-container {
+        .alttab-spec-card-container {
             background-color: white;
             margin: 5px;
             border-radius: 5px;
             box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.5);
             overflow: hidden;
+            color: black;
         }
-        .article-container {
+        .alttab-spec-card-container a {
+            color: black;
+        }
+        .alttab-spec-article-container {
             height: 120px;
         }
-        .article-container p {
+        .alttab-spec-article-container p {
             margin: 0.2em;
         }
-        .article-meta {
+        .alttab-spec-article-meta {
             font-size: 0.8em;
             opacity: 0.7;
         }
-        .article-data-left {
+        .alttab-spec-article-data-left {
             width: 65%;
             float: left;
             padding: 5px;
         }
-        .article-title {
+        .alttab-spec-article-title {
             font-weight: bold;
         }
 
-        .article-image-right {
+        .alttab-spec-article-image-right {
             width: 35%;
             height: 100%;
             float: left;
             overflow: hidden;
         }
-        .article-image-right > img {
+        .alttab-spec-article-image-right > img {
             height: 100%;
             object-fit: cover;
         }
